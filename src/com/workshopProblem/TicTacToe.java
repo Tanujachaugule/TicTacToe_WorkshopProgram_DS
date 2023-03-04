0e43f9a9777d191ff99fca1a930483cc607f85c2
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-03-04 18:54:40 +0530

    Added: Function to Create the Board

--- src/com/workshopProblem/TicTacToe.java
+++ src/com/workshopProblem/TicTacToe.java
@@ -3,5 +3,19 @@ package com.workshopProblem;
 public class TicTacToe {
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
+        System.out.println();
+        PlayingTTT play = new PlayingTTT();
+        String[][] board = new String[3][3];
+        play.settingBoard(board);
     }
 }
+class PlayingTTT {
+    public void settingBoard(String[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = ". ";
+            }
+        }
+    }
+}
+
