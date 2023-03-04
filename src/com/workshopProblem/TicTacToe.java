62a1854e674acf2cffd803caf6e761c4222f458c
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-03-04 19:20:02 +0530

    Added: Function to Changed the Name Of Computer and Player

--- src/com/workshopProblem/TicTacToe.java
+++ src/com/workshopProblem/TicTacToe.java
@@ -1,20 +1,36 @@
 package com.workshopProblem;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
         System.out.println();
-         final PlayingTTT play = new PlayingTTT();
-         final String[][] board = new String[3][3];
+        final PlayingTTT play = new PlayingTTT();
+        Scanner sc = new Scanner(System.in);
+        final String[][] board = new String[3][3];
         play.settingBoard(board);
-        String firstPlay = " ";
-        if(play.decidingToss()){
-            firstPlay = firstPlay+"player";
-        }else{
-            firstPlay = firstPlay+"computer";
+        String player;
+        String cpu;
+        if (play.decidingToss()) {
+            System.out.println("Enter a character between X or O");
+            player = sc.next();
+            if (player == "X") {
+                cpu = "O";
+            } else {
+                cpu = "X";
+            }
+        } else {
+            if (play.decidingToss()) {
+                cpu = "O";
+                player = "X";
+            } else {
+                cpu = "X";
+                player = "O";
+            }
         }
+        sc.close();
     }
 }
 class PlayingTTT {
